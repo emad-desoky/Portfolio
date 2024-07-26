cff19a0e06bc45a5e7d2d6824a41e33380a37ab3
emad-desoky <deskoemad@gmail.com>
2024-07-27 02:40:23 +0300

    header done

--- 04-appointment/components/header/Header.jsx
+++ 04-appointment/components/header/Header.jsx
@@ -176,7 +176,7 @@ export default function Header() {
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3058.7401693454184!2d-104.89208368459286!3d39.60668177945741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c79950489b0f3%3A0x6a2a1d1e90cc0a67!2s5670%20Greenwood%20Plaza%20Blvd%2C%20Suite%20404%2C%20Greenwood%20Village%2C%20CO%2080111!5e0!3m2!1sen!2sus!4v1615924819167!5m2!1sen!2sus"
           width="100%"
-          height="450"
+          height="350"
           style={{ border: 0 }}
           allowFullScreen=""
           loading="lazy"
