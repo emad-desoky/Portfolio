6de919c0ef1390edabfe5f878f3db8fba5d52ecc
emad-desoky <deskoemad@gmail.com>
2024-08-01 23:07:18 +0300

    updated

--- 04-Dental Website/pages/index.jsx
+++ 04-Dental Website/pages/index.jsx
@@ -1,14 +1,15 @@
 "use client";
-import axios from "axios";
-import { useEffect } from "react";
-import Joinus from "@/components/joinus/Joinus";
+
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
 import ClinicNavBar from "@/components/NavBar/ClinicNavBar";
-import MainApp from "@/components/main-app/MainApp";
 
 export default function Home() {
   return (
     <>
-      <MainApp />
+      <ClinicNavBar />
+      <Header />
+      <Footer />
     </>
   );
 }
