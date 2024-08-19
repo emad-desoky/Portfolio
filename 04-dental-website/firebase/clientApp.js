abe4c505d5d219058391d260cbdb952a18288208
emad-desoky <deskoemad@gmail.com>
2024-08-20 02:24:01 +0300

    authentcaaton done

--- 04-dental-website/firebase/clientApp.js
+++ 04-dental-website/firebase/clientApp.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const clientCredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "kAIzaSyCdcGBP99aiCKjhaSVEePxVdtjGck1cxY",
+  authDomain: "dental-website-cf2a5.firebaseapp.com",
+  projectId: "dental-website-cf2a5",
+  storageBucket: "dental-website-cf2a5.appspot.com",
+  messagingSenderId: "582437244163",
+  appId: "1:582437244163:web:c85d4afa071a0045c2ad36",
 };
 
 if (!firebase.apps.length) {
